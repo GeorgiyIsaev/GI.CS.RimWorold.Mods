79d99b778022b2328530ae5463a183ae418bb00c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-08 14:05:23 +0300

    Условие срабатвает каждые 2 часа

--- LearningModBuildings/LearningModBuildings.EventMod/Events/GameCondition_TestCondition.cs
+++ LearningModBuildings/LearningModBuildings.EventMod/Events/GameCondition_TestCondition.cs
@@ -18,9 +18,28 @@ namespace LearningModBuildings.EventMod.Events
         public override void GameConditionTick()
         {
             base.GameConditionTick();
-            //проверка когда событие было вызвано
-            Log.Message($"1");
+            //Что будит проиходть каждый тик
+           
+            /*Обаботаем все карты через некоторое время*/
+            if(Find.TickManager.TicksGame % 5000 == 0)
+            {
+                //2500 тиков == 1 час игрового времени
+                DoEffectTick();
+            }
         }
+        private void DoEffectTick()
+        {
+            Log.Message($"Tick");
+            foreach (var map in AffectedMaps) {
+                foreach (var pawn in map.mapPawns.AllPawns)
+                {
+                    //Наносить пешке 5 урона
+                    pawn.TakeDamage(new DamageInfo(DamageDefOf.Stab, 5f));
+
+                }
+            }
+        }
+
         public override void End()
         {
             base.GameConditionTick();
