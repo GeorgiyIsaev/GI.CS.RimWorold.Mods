71fc191e46354545bad2a8a97fb8c009ed7615d2
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-08 13:21:46 +0300

    Описание условия

--- LearningModBuildings/LearningModBuildings.EventMod/Events/GameCondition_TestCondition.cs
+++ LearningModBuildings/LearningModBuildings.EventMod/Events/GameCondition_TestCondition.cs
@@ -3,10 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
 
 namespace LearningModBuildings.EventMod.Events
 {
     public class GameCondition_TestCondition : GameCondition
     {
+        public override void Init()
+        {
+            /*Инциализация осбытия**/
+            base.Init();
+            Log.Message($"{def.defName} execute");
+        }
+        public override void GameConditionTick()
+        {
+            base.GameConditionTick();
+            //проверка когда событие было вызвано
+            Log.Message($"1");
+        }
+        public override void End()
+        {
+            base.GameConditionTick();
+            //Событие завершилось
+            Log.Message($"END");
+        }
+
     }
 }
