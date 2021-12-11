3dbaf5b85d05754358bee75ea53f96d46afa7d68
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-11 23:53:25 +0300

    Класс своств и событий

---
+++ LearningModBuildings/LearningModBuildings.HediffMod/HeddiffCompProperties_TestHediff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+//HEalthDIFFerence (from normal state)
+//Различие в состоянии здоровья
+
+namespace LearningModBuildings.HediffMod
+{
+    //Класс свойств
+    public class HeddiffCompProperties_TestHediff : HediffCompProperties
+    {
+        //Можно сразу же задать настройки 
+        public float TestParam;
+        //Теперь в XML в списке comps можно сразу определить его использвоание
+        /*  <li Class="LearningModBuildings.HediffMod.HeddiffCompProperties_TestHediff">
+            <TestParam>2</TestParam>
+      </li>
+         */
+
+        public HeddiffCompProperties_TestHediff()
+        {
+         
+
+
+            
+            //В конструкторе необходимо указать тип нашего обрабочика
+            compClass = typeof();
+
+
+        }
+    }
+}
