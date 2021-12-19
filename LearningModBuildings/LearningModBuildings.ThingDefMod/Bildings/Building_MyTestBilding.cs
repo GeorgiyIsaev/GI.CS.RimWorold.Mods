b05d726908fd903a07f9fce151bf7e734c2bbff5
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-19 16:38:55 +0300

    add class Building_MyTestBilding

---
+++ LearningModBuildings/LearningModBuildings.ThingDefMod/Bildings/Building_MyTestBilding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace LearningModBuildings.HediffMod.Bildings
+{
+    class Building_MyTestBilding : Building
+    {
+
+    }
+}
