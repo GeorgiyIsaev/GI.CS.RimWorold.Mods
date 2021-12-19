5a3eae440a35736bf96e77b173f741ca71cc256d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-19 17:00:39 +0300

    add methods

--- LearningModBuildings/LearningModBuildings.ThingDefMod/Bildings/Building_MyTestBilding.cs
+++ LearningModBuildings/LearningModBuildings.ThingDefMod/Bildings/Building_MyTestBilding.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,55 @@ namespace LearningModBuildings.HediffMod.Bildings
 {
     class Building_MyTestBilding : Building
     {
+        /*Метод для генирации предмета*/
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {            
+            base.SpawnSetup(map, respawningAfterLoad);
+            Log.Message("SpawnSetup");
+        }
 
+        /*Изменение предмета при кажом тике*/
+        public override void Tick()
+        {
+            base.Tick();
+            Log.Message("TICK");
+        }
+
+        /*Выпадающий список из предмета*/
+        public override IEnumerable<FloatMenuOption> GetFloatMenuOptions(Pawn selPawn)
+        {
+            /*При выборе данной опции наносит урон пешке*/
+            yield return new FloatMenuOption("Option 1", delegate
+            {
+                selPawn.TakeDamage(new DamageInfo(DamageDefOf.Bite, 20));
+            });
+        }
+        private FloatMenuOption GetOption()
+        {
+            FloatMenuOption option = new FloatMenuOption("Option2",
+                delegate 
+                {
+                    Log.Message("Option2"); ;
+                });
+            return option;
+        }
+
+        /*Меню придмета при его выборе (Гизма)*/
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            yield return new Command_Action()
+            {
+                defaultLabel = "Default Labael",
+                defaultDesc = "Default desc 123",
+                icon = def.uiIcon,
+                action = delegate
+                {
+                    Find.TickManager.DebugSetTicksGame
+                    (
+                        Find.TickManager.TicksGame + 12000000
+                    );
+                }
+            };
+        }
     }
 }
