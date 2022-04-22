3278f20a92acdd478b014d0305ca68dd792f688e
Георгий Исаев <georgiyelbaf@gmail.com>
2022-04-23 00:21:27 +0300

    Удвоение предмета строением

--- LearningModBuildings/LearningModBuildings.ThingJobApparel/Bildings/Building_MyTestBilding.cs
+++ LearningModBuildings/LearningModBuildings.ThingJobApparel/Bildings/Building_MyTestBilding.cs
@@ -23,21 +23,58 @@ namespace LearningModBuildings.HediffMod.Bildings
                 }
                 return false;
             }
-        }
-        public Thing ContainedThing; //переменная в котороую мы будим складывать наш предмет
-
-
+        } 
+        
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
             base.SpawnSetup(map, respawningAfterLoad);
 
             compPowerTrader = GetComp<CompPowerTrader>();
         }
+       
+        public Thing ContainedThing; //переменная в котороую мы будим складывать наш предмет
+
+        public bool Complete; //будит проверять выполнена ли работа
+
+        private int ticks => 10000; // сколько  нужно времени что бы предмет улучшился
+        private int ticker = 0; //таймер тикера //обязательно сохранить в ExposeData
+
+        public override void Tick()
+        {
+            base.Tick();
+            //Если еще не выполнено движем таймер
+            if (!Complete)
+            {
+                ticker--; 
+                if(ticker <= 0)
+                {              
+                    ThingComplate();
+                }
+            }
+        }
+        private void ThingComplate()
+        {
+            ticker = ticks; // возвращаем стартовое значение тикера
+
+            Complete = true; //работа завершилась
+
+            /*Проверим что контейнер есть*/        
+            if(ContainedThing != null)
+            {    /*Умножаем количество предметов контейнера на 2*/
+                ContainedThing.stackCount *= 2;
+            }
+
+        }
+
+
+
+
 
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_References.Look(ref ContainedThing, "ContainedThing");
+            Scribe_Values.Look(ref ticker, "ticker"); //сохраняем значение
         }
      
 
