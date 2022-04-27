bacedc6711e51afbf642c14ec8aba2d9dc81ba6f
Георгий ГераМера <germesutan@gmail.com>
2022-04-27 14:41:27 +0300

    class Bullet_ExplosiveBullet (Будит взрыватся при попадании)

---
+++ LearningModBuildings/LearningModBuildings.ThingJobApparel/Bullet_ExplosiveBullet.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningModBuildings.HediffMod
+{
+    public class Bullet_ExplosiveBullet : Bullet
+    {
+    }
+}
