c390f383161c38ffaaf68731ccaea047e71d777d
Георгий ГераМера <germesutan@gmail.com>
2022-04-27 14:47:07 +0300

    класс для вызрыва

--- LearningModBuildings/LearningModBuildings.ThingJobApparel/Bullet_ExplosiveBullet.cs
+++ LearningModBuildings/LearningModBuildings.ThingJobApparel/Bullet_ExplosiveBullet.cs
@@ -3,10 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
 
 namespace LearningModBuildings.HediffMod
 {
     public class Bullet_ExplosiveBullet : Bullet
     {
+        /*Переопределим логику попадания*/
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing); //оставляем базовую реализацию
+
+            if(hitThing != null) //првоеряем что попали
+            {
+                //добавляем взрыв
+                GenExplosion.DoExplosion(hitThing.Position, hitThing.Map, 
+                    Rand.Range(1, 3),
+                    DamageDefOf.Bomb, launcher);
+
+            }
+        }
     }
 }
